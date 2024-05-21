9d574a547bc7f90a876473967b594fba270cfc5e
mohd noman <mohdnoman2751@gmail.com>
2024-05-21 23:29:08 +0530

    Implemented JWT authentication and password hashing.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -57,4 +59,34 @@ const UserSchema = new mongoose.Schema({
     }
 }, { timestamps: true })
 
-export const User = mongoose.model('User', UserSchema)
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        { userId: this._id }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+}
+
+UserSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        { userId: this._id }, 
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+}
+
+export const User = mongoose.model('User', UserSchema);
